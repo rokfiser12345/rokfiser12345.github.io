1b74b40b49c020244e60be17f88070764baa3951
rokfiser12345 <rok.fiser@student.um.si>
2022-12-27 15:46:23 +0100

    Updated domov added copy script

---
+++ copyFiles.py
@@ -0,0 +1,4 @@
+import shutil
+
+newPath=shutil.copy(r'B:\myFirstWebsite\rokfiser12345.github.io\Domov.html',r'B:\IntervencijeTool')
+newPath=shutil.copy(r'B:\myFirstWebsite\rokfiser12345.github.io\blog\blog.html',r'B:\IntervencijeTool\blog')
